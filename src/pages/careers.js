66338a5e28349367de69f308b759775e16775909
Gabi <g.federico.rodriguez@gmail.com>
2021-04-11 16:56:55 -0300

    careers

---
+++ src/pages/careers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SEO from "../components/seo"
+
+import Layout from '../components/layout'
+import Testimonials from '../components/Testimonials'
+
+const CareersPage = () => {
+  return (
+    <Layout background="true">
+      <SEO title="Trips" />
+      <Testimonials/>
+    </Layout>
+  )
+}
+
+export default CareersPage
